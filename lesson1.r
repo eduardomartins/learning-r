6d0bd341c58521b039f557c6b0662c9e72ba096c
Eduardo Sant'Anna Martins <eduardo@eduardomartins.site>
2018-04-25 17:02:11 -0300

    First Lesson
    Check out full example (in Portuguese): https://eduardomartins.site/blog/post/linguagem-r-tipos-de-dados/

---
+++ lesson1.r
@@ -0,0 +1,41 @@
+# Logical Type
+log1<-TRUE
+log2<-FALSE
+
+print("Logical: ")
+print(log1)
+print(log2)
+
+# Integer Type
+iv<-76L
+
+print("Integer: ")
+print(iv)
+
+# Numeric type
+nv1<-34.5
+nv2<-928
+
+print("Numeric: ")
+print(nv1)
+print(nv2)
+
+# Complex Type
+cv<-5+3i
+
+print("Complex: ")
+print(cv)
+
+#C Character type
+chv1<-"Name"
+chv2<-'N'
+
+print("Character: ")
+print(chv1)
+print(chv2)
+
+# Raw Type
+rv<-charToRaw("ç")
+
+print("Raw: ")
+print(rv)
